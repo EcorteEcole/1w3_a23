484f0244049e3f16abc958a6aece84fe81934bf0
Ecorte <antoine.turgeon2005@hotmail.com>
2023-12-06 11:35:41 -0500

    push

--- projet_02/js/index.js
+++ projet_02/js/index.js
@@ -1,22 +1,20 @@
 /* eslint-disable no-undef */
 const fillForm = () => {
-	const cout = Number.parseFloat(document.querySelector('#cout').value);
-	const mise = Number.parseFloat(document.querySelector('#mise').value);
+	const cout = document.getElementById('cout');
+	const mise = document.getElementById('mise');
 
-	document.querySelector('#montant').value = (cout - mise).toString();
-	document.querySelector('#pourcent').innerText = `(${((mise / cout) * 100).toFixed(2)}%)`;
+	document.getElementById('montant').value = (cout - mise).toString();
+	document.getElementById('pourcent').innerText = `(${((mise / cout) * 100).toFixed(2)}%)`;
 };
 
 const getResult = () => {
-	const montant = Number.parseFloat(document.querySelector('#montant').value);
-	const taux /* i */ = Number.parseFloat(document.querySelector('#taux').value) / 100 / 12;
-	const paiment /* n */ =
-		Number.parseFloat(document.querySelector('#amortissement').value) *
-		Number.parseFloat(document.querySelector('#frequence').value);
+	const montant = document.getElementById('montant').value;
+	const taux /* i */ = document.getElementById('taux').value / 100 / 12;
+	const paiment /* n */ = document.getElementById('amortissement').value * document.getElementById('frequence').value;
 
 	const numerator = montant * taux * (1 + taux) ** paiment;
 	const denominator = (1 + taux) ** paiment - 1;
-	document.querySelector('#result').value = (numerator / denominator).toFixed(2).toString();
+	document.getElementById('#result').value = (numerator / denominator).toFixed(2).toString();
 };
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
@@ -27,5 +25,5 @@ const submit = (event) => {
 };
 
 document.addEventListener('DOMContentLoaded', () =>
-	document.querySelector('#submitButton').addEventListener('click', submit),
+	document.getElementById('submitButton').addEventListener('click', submit),
 );
